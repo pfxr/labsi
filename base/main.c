8c0a36e998c6a69ad41ce7d67821724040699175
joaodh92 <joao92.c@gmail.com>
2014-06-22 19:44:18 +0100

    pronto

--- base/main.c
+++ base/main.c
@@ -5,40 +5,45 @@
 #include <string.h>
 #include "../nrf24.h"
 
-#define pedro1 {0xD7,0xD7,0xD7,0xD7,0xD7}
+#define pedro1 {0xD7,0xD7,0xD7,0xD7,0xD7}  //define dos mac de cada modulo RF
 #define pedro2 {0xE7,0xE7,0xE7,0xE7,0xE7}
 #define joao1  {0xA7,0xA7,0xA7,0xA7,0xA7}
 #define joao2  {0xB7,0xB7,0xB7,0xB7,0xB7}
+#define base   {0xC8,0xC8,0xC8,0xC8,0xC8}
 
-#define FOSC 8000000
-#define F_CPU 8000000
-#define BAUD 9600
-#define USART_UBBR_VALUE FOSC/16/BAUD-1
+#define FOSC 1000000
+#define F_CPU 1000000
+#define BAUD 4800
+#define USART_UBBR_VALUE FOSC/16/BAUD-1 //calculo para o valor de ubrr da usart
 
 //char buffer_Tx[200];
-volatile unsigned char rx;
+volatile unsigned char rx,prontos=0,comecou=0;
+
 
 char data_array[4],cont_players=0,buffer[40],buffer_rx[15];
 
-volatile char cont_rx=0,StrRxFlag=0;
+volatile char StrRxFlag=0,cont=0,cont1=10;
+unsigned char cont_rx=0;
 
-uint8_t rx_address[5] = joao1;
-uint8_t tx_address[5] = pedro1;
+uint8_t rx_address[5] = base;
+uint8_t tx1_address[5] = pedro1;
+uint8_t tx2_address[5] = pedro2;
 
 void init (void)
 {
-    DDRB|=0x01;
-    UBRR0H = (unsigned char)(USART_UBBR_VALUE>>8);
+    DDRC|=(1<<PC5);
+    UBRR0H = (unsigned char)(USART_UBBR_VALUE>>8); //insere o valor calculado previamente
     UBRR0L = (unsigned char) (USART_UBBR_VALUE);
 
-    UCSR0C= 0b00000110;
-    UCSR0B= 0b00011000;
-    UCSR0B|=0x80;
+    UCSR0C|=(1<<UCSZ01)|(1<<UCSZ00); //usart configurada com 8bit de dado e 1 stopbit
+    UCSR0B|=(1<<RXCIE0)|(1<<RXEN0)|(1<<TXEN0); //activa a interruopçao de recepçao completa e a transmissao e recepcao activas
 
-    SREG |= 0x80;			//Autorizacao global das interrupcoes
+    sei();			//Autorizacao global das interrupcoes
 
 }
-void nrf_receber()
+
+
+void nrf_receber() //funçao igual a existente no codigo da pistola
 {
     if(nrf24_dataReady())
     {
@@ -49,24 +54,23 @@ void nrf_receber()
 
 
 
-void enviar(char *Tx)
+void enviar(char *Tx) //funcao para enviar pela usar para o pc
 {
-    int i=0;
-    while(i<=strlen(Tx))
+    unsigned char i=0;
+    PORTC|=(1<<PC5);
+    while(i<strlen(Tx))
     {
         while(!(UCSR0A & (1<<UDRE0)));
-        UDR0=Tx[i];
+        UDR0=Tx[i]; //envia caracter a caracter pela usart até que i atinja o tamnho da string-1
         i++;
     }
-
-
-
+    PORTC&=~(1<<PC5);
 }
 
-void nrf_enviar(char buff[])
+void nrf_enviar(char buff[]) //função igual ao codigo da pistola
 {
-
-    char tamanho=0,i=0,temp;
+    unsigned char i=0;
+    char tamanho=0,temp;
     tamanho=strlen(buff);
     while(i<tamanho)
     {
@@ -84,11 +88,11 @@ void nrf_enviar(char buff[])
 
         if(temp == NRF24_TRANSMISSON_OK)
         {
-              //enviar("> Tranmission went OK\r\n");
+            //enviar("> Tranmission went OK\r\n");
         }
         else if(temp == NRF24_MESSAGE_LOST)
         {
-           // enviar("> Message is lost ...\r\n");
+            // enviar("> Message is lost ...\r\n");
         }
     }
 
@@ -100,45 +104,81 @@ void nrf_enviar(char buff[])
 
 
     _delay_ms(10);
-    // enviar("nrf a enviar\r\n");
 }
-void processar_RX(char rx[])
+
+void clear_dataarray() //funcao para limpar o data array
+{
+    unsigned char i=0;
+    for(i=0; i<4; i++)
+        data_array[i]='0';
+}
+
+void processar_RX(char rx[])  //função de incio de jogo, processa os comandos vindos pela usart
 {
     char buffer_Tx[10],buff[10];
-    char i;
-    // enviar("Entrei no Rx\r\n");
+    unsigned char i;
+
     switch (rx[0])
     {
     case '1':
-    {   //enviar("entrei no case1");
+    {
+        nrf24_tx_address(tx1_address);//envia para os 2 jogadores 34 a indicar que a base esta pronta
         nrf_enviar("34\r\n");
-        do//Espera até que player1 ou player2 enviem '4' (inicio de jogo/disparo)
+        nrf24_tx_address(tx2_address);
+        nrf_enviar("34\r\n");
+        do
         {
-            //    enviar("estou a espera de receber14\r\n");
-            nrf_receber();
+            clear_dataarray();
+            do//Espera até que player1 ou player2 enviem '4' (inicio de jogo/disparo)
+            {
+                nrf_receber();
+            }
+            while(data_array[0]=='0');
+            //  enviar("estou a espera de receber1");
+            if((data_array[0]=='1') && (data_array[1]=='4'))
+            {
+                enviar("14\r\n"); //envia para a aplicao a indicar que o jogador 1 esta pronto
+                prontos++;
+            }
+            else
+            {
+                if((data_array[0]=='2') && (data_array[1]=='4'))
+                {
+                    enviar("24\r\n");//envia para a aplicao a indicar que o jogador 2 esta pronto
+                    prontos++;
+                }
+            }
 
+            for (i=0; i<strlen(buffer_Tx); i++)
+            {
+                data_array[i]=0;	//limpa o buffer
+            }
         }
-        while(data_array[0]!='1');
-        //  enviar("estou a espera de receber1");
-        if((data_array[0]=='1') && (data_array[1]=='4'))
-        {
-            //  enviar("estou a espera de receber14");
-            sprintf(buffer_Tx,"%c%c32%c",data_array[0],data_array[1],'<');
-            enviar(buffer_Tx);
-        }
-        for (i=0; i<strlen(buffer_Tx); i++)
-        {
-            data_array[i]=0;	//limpa o buffer
-        }
+        while(prontos!=2);
         break;
     }
     case '2':
     {
-        PORTB=0x01;
-        sprintf(buff,"13%s",(rx+1));
-        nrf_enviar(buff);
-        // enviar(buff);
-        break;
+        if(rx[1]=='1')
+        {
+            nrf24_tx_address(tx1_address);
+            PORTB=0x01;
+            sprintf(buff,"13%s",(rx+2)); //a aplicação envia para o player o nome, o player e
+            nrf_enviar(buff);           // o player e defenido pelo rx[1]
+            break;
+        }
+        else
+        {
+            if(rx[1]=='2')
+            {
+                nrf24_tx_address(tx2_address);
+                PORTB=0x01;                 //analoo ao if de cima mas agora e o outro player
+                sprintf(buff,"23%s",(rx+2));
+                nrf_enviar(buff);
+                break;
+            }
+        }
+        comecou=1;
     }
     }
 
@@ -148,92 +188,50 @@ void processar_RX(char rx[])
 
 ISR (USART_RX_vect)
 {
-    buffer_rx[cont_rx]=UDR0;         //Read USART data register
-    if(buffer_rx[cont_rx]=='\n')   //check for carriage return terminator and increment buffer index
+    PORTC|=(1<<PC5);  //indica no led que esta a receber dados da usar
+    buffer_rx[cont_rx]=UDR0;        //coloca o caracter no vector buffer
+    if(buffer_rx[cont_rx]=='\n')   //a leitura da string recebida termina quando receber o caracter \n
     {
-        // if terminator detected
-        //Set String received flag
-        //buffer_rx[cont_rx-1]=0x00;   //Set string terminator to 0x00
-        cont_rx=0;                //Reset buffer index
+        cont_rx=0;                //lipa o index do vector
         //  enviar(buffer_rx);
-        processar_RX(buffer_rx);//enviar(buffer_rx);
-        buffer_rx[0]='\0';
+        PORTC&=~(1<<PC5);  //indica que terminou de receber dados da suart
+        processar_RX(buffer_rx); //procesa os dados recebidos chamando a funcao
+        buffer_rx[0]='\0';    //limpar ao buffer
 
     }
-    else cont_rx++;
+    else cont_rx++; //incrementa o indice do vector
 
 }
 
-void clear_dataarray()
-{
-    char i=0;
-    for(i=0; i<4; i++)
-        data_array[i]='0';
-}
+
 
 void dados_recebidos()
 {
-    char buff[10];
+    char buff[5];
+
     clear_dataarray();
-    while(data_array[0]=='0')
+    do
     {
         nrf_receber();
     }
-    switch(data_array[0])
-    {
-    case '1':
-    {
-        if(data_array[1]=='1')
-        {
-            sprintf(buff,"%c%c%c%c%c",data_array[0],data_array[1],data_array[2],data_array[3],'<');
-            enviar(buff);
-        }
-        else
-        {
-            if(data_array[1]=='2')
-            {
-                sprintf(buff,"vida %c%c\r\n",data_array[2],data_array[3]);
-                enviar(buff);
-            }
-        }
-        break;
-    }
-
-    case '2':
-    {
-        if(data_array[1]=='1')
-        {
-            sprintf(buff,"municoes %c%c\r\n",data_array[2],data_array[3]);
-            enviar(buff);
-        }
-        else
-        {
-            if(data_array[1]=='2')
-            {
-                sprintf(buff,"vida %c%c\r\n",data_array[2],data_array[3]);
-                enviar(buff);
-            }
-        }
-        clear_dataarray();
-        break;
-    }
-    }
+    while(data_array[0]=='0');
+    sprintf(buff,"%s%c",data_array,'<');//pega no data array e coloca o caracter < para  app ler ate esse ponto
+    enviar(data_array); //envia para a app pela usart
 
 }
 
 int main(void)
 {
     init();
-    nrf24_init();
-    nrf24_config(2,4);
+    nrf24_init(); //inicia o modlo nrf
+    nrf24_config(2,4); //configura para o chanel 2 e o tamanho do buffer 4
     buffer_rx[0]=0;
-    nrf24_tx_address(tx_address);
-    nrf24_rx_address(rx_address);
-    enviar("Setup concluido");
+    nrf24_rx_address(rx_address); //define o mac rx do modulod
+    enviar("Setup concluido\r\n");
     clear_dataarray();
     while(1)
     {
-        dados_recebidos();
+        if(comecou==1)// se o jogo tiver comecado chama a funcao
+            dados_recebidos();
     }
 }
-
